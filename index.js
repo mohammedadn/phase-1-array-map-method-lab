405c9b36bdf3812ca204595d72985663446d2d10
Mohammedadn <mohammedaden023@gmail.com>
2024-03-30 23:22:04 +0300

    firstttt

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((line) => {
+    const tokens = line.split(" ")
+    let capitalizedTokens = tokens.map(
+      (token) => token.charAt(0). toUpperCase() + token.slice(1)
+    );
+    let response = capitalizedTokens.join(" ");
+    return response;
+  });
 }
